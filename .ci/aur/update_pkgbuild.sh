ef48e560792f4efce6fa7fc776263af7f7f7e0d1
pierre <dommerc.pierre@gmail.com>
2023-01-09 01:23:48 +0100

    wip

--- .ci/aur/update_pkgbuild.sh
+++ .ci/aur/update_pkgbuild.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-ls -la
+green="\e[38;5;2m"
+bold="\e[1m"
+reset="\e[0m"
 
 pkgver="${RELEASE_TAG#v}"
 pkgbuild=.ci/aur/PKGBUILD
 
 # bump the package version
 sed -i "s/pkgver=.*/pkgver=$pkgver/" $pkgbuild
+printf "%b%b✓%b bump version to $RELEASE_TAG\n" "$green" "$bold" "$reset"
 
 # generate the new checksum
 sum=$(sha256sum "$PKG_NAME-v$pkgver".tar.gz | awk '{print $1}')
-echo "sha256sum: $sum"
 sed -i "s/sha256sums=('.*')/sha256sums=('$sum')/" $pkgbuild
+printf "%b%b✓%b generated checksum $sum\n" "$green" "$bold" "$reset"
